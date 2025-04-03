f8b7ceb13c3409a3de9d3737d9ea203d38eb47d3
BH Y <hesskuro@gmail.com>
2025-04-03 19:46:45 +0800

    流式应答接口实现
    1. 实现了流式应答接口
    2. 修正了错误的结构，解决了无法扫描的问题

---
+++ mh-dev-tech-app/src/main/java/com/masterhesse/mh/dev/tech/Application.java
@@ -0,0 +1,15 @@
+package com.masterhesse.mh.dev.tech;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication(scanBasePackages = "com.masterhesse.mh.dev.tech")
+@Configurable
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
