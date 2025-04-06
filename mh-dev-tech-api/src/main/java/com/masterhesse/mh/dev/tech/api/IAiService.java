6428947d247f32d1188c63019693b8a4723304db
BH Y <hesskuro@gmail.com>
2025-04-06 16:15:13 +0800

    扩展OpenAI模型

--- mh-dev-tech-api/src/main/java/com/masterhesse/mh/dev/tech/api/IAiService.java
+++ mh-dev-tech-api/src/main/java/com/masterhesse/mh/dev/tech/api/IAiService.java
@@ -7,4 +7,5 @@ import reactor.core.publisher.Flux;
 public interface IAiService {
     ChatResponse generate(String model, String message);
     Flux<ChatResponse> generateStream(String model, String message);
+    Flux<ChatResponse> generateStreamRag(String model, String ragTag,String message);
 }
